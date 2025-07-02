0ca75083297480c2cf567973103307d75d04079b
aahsnr <ahsanur041@proton.me>
2025-07-02 19:32:49 +0600

    updated\

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ bash $HOME/git/tumbleweed-setup/setup-scripts/03git
 bash $HOME/git/tumbleweed-setup/setup-scripts/04syspkgs
 bash $HOME/git/tumbleweed-setup/setup-scripts/05desktop
 bash $HOME/git/tumbleweed-setup/setup-scripts/06editors
-bash $HOME/git/tumbleweed-setup/setup-scripts/07opipkgs
-bash $HOME/git/tumbleweed-setup/setup-scripts/08asus
-bash $HOME/git/tumbleweed-setup/setup-scripts/09flatpak
-bash $HOME/git/tumbleweed-setup/setup-scripts/10rustpkgs
+bash $HOME/git/tumbleweed-setup/setup-scripts/07pypkgs
+bash $HOME/git/tumbleweed-setup/setup-scripts/08opipkgs
+bash $HOME/git/tumbleweed-setup/setup-scripts/09asus
+bash $HOME/git/tumbleweed-setup/setup-scripts/10flatpak
+bash $HOME/git/tumbleweed-setup/setup-scripts/11rustpkgs
+bash $HOME/git/tumbleweed-setup/setup-scripts/12fonts
+bash $HOME/git/tumbleweed-setup/setup-scripts/13caelestia
+bash $HOME/git/tumbleweed-setup/setup-scripts/14plugins
 
 echo "Installing Packages from Source"
 # sudo bash ~/tumbleweed-setup/source_builds/app2unit
